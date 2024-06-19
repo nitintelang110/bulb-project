9ab02a6561ae32e459f26b94d01e57976537d769
nitin telang <nitintelang110@gmail.com>
2024-06-19 10:36:30 +0530

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const text1 = document.getElementById('demo')
+const text2 = document.getElementById('body')
+
+function bulbOn() {
+  text1.src = '	https://www.w3schools.com/js/pic_bulboff.gif'
+ 
+}
+
+function bulbOff() {
+    text1.src='	https://www.w3schools.com/js/pic_bulbon.gif'
+    text2.styles.backgroundColor = yellow;
+  }
+  
